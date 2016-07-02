f49a8465062aa2eabbb787116a2c1bcf8d0ecabb
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2016-07-02 08:41:33 -0300

    I was told that extending ActiveSupport::Concern fixes it
    And it indeed fixes it

--- lib/streaming_support.rb
+++ lib/streaming_support.rb
@@ -1,3 +1,8 @@
 module StreamingSupport
+  extend ActiveSupport::Concern # required for it to work in Rails 5
   include ActionController::Live
+  # Alternatively:
+  # def self.included(base)
+  #   base.include ActionController::Live
+  # end
 end
